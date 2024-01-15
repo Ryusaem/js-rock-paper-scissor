7878d00ca9efa35d65f9443af7e6501dbabed44b
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2024-01-15 20:49:42 +0100

    v2

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ let playerChoice = "";
 let computerChoice = "";
 let playerScore = 0;
 let computerScore = 0;
+let choice = ["fire", "plant", "water"];
 
 let rounds = document.querySelector("#rounds");
-let choice = ["fire", "plant", "water"];
 let sentence = document.querySelector("#sentence");
 let state = document.querySelector("#state");
+let computer_score = document.querySelector("#computerScore");
+let player_score = document.querySelector("#playerScore");
 
 let enemy = document.querySelector("#enemy");
 const pokemonButtons = document.querySelectorAll(".pokemon");
@@ -29,17 +31,13 @@ function updateScoreAndState(winner, playerSelection, computerSelection) {
   if (winner === "player") {
     playerScore++;
 
-    document.querySelector(
-      "#playerScore"
-    ).textContent = `Player: ${playerScore}`;
+    player_score.textContent = `Player: ${playerScore}`;
 
     state.textContent = "You WIN";
   } else if (winner === "computer") {
     computerScore++;
 
-    document.querySelector(
-      "#computerScore"
-    ).textContent = `Computer: ${computerScore}`;
+    computer_score.textContent = `Computer: ${computerScore}`;
 
     state.textContent = "You LOSE";
   } else {
@@ -87,7 +85,18 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+function showPopup(message) {
+  document.getElementById("messageContent").textContent = message;
+  document.getElementById("popupMessage").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popupMessage").style.display = "none";
+}
+
 function restartGame() {
+  playerScore === 5 ? showPopup("You win!") : showPopup("You lose!");
+
   playerChoice = "";
   computerChoice = "";
 
@@ -96,28 +105,17 @@ function restartGame() {
   computerScore = 0;
 
   rounds.innerText = `Round: ${round}`;
-  document.querySelector("#playerScore").textContent = `Player: ${playerScore}`;
-  document.querySelector(
-    "#computerScore"
-  ).textContent = `Computer: ${computerScore}`;
-  computer_choice.textContent = "";
-  state.textContent = "";
+  player_score.textContent = `Player: ${playerScore}`;
+  computer_score.textContent = `Computer: ${computerScore}`;
+  computer_choice.textContent = "None";
+  state.textContent = "Idle";
   sentence.textContent = "";
-
-  alert("The game is going to restart.");
 }
 
 pokemonButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (isGameOver()) {
-      restartGame();
-    }
-
     playerChoice = e.target.id || e.srcElement.id;
     computerChoice = getComputerChoice();
-
-    console.log(`playerChoice: ${playerChoice}`);
-    console.log(`computerChoice: ${computerChoice}`);
 
     playRound(playerChoice, computerChoice);
 
